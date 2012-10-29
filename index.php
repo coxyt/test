21188e36118e658536e90a3a5e1edfbbd2c03413
Evgeniy Naidenov <3064709@gmail.com>
2012-10-29 19:02:28 +0200

    My first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "This is index.php";
+echo __FILE__;
+
+class MyClass {
+	function __construct() {
+		return null;
+	}
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+echo "This is test";
+echo __FILE__;
+echo "New line";
+?>
